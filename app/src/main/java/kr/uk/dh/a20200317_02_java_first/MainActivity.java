a08358a3606f61c1620d5a3214b1f3a243fbd50f
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-17 15:03:14 +0900

    데이터바인딩 세팅

--- app/src/main/java/kr/uk/dh/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/kr/uk/dh/a20200317_02_java_first/MainActivity.java
@@ -1,14 +1,28 @@
 package kr.uk.dh.a20200317_02_java_first;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import kr.uk.dh.a20200317_02_java_first.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding = null;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    binding.inputEdt
     }
 }
