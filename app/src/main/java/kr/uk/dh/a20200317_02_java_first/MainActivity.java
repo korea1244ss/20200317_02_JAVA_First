ccde31fe97562efff6584f78a9bc927e047d4f43
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-17 14:32:35 +0900

    Initial commit

---
+++ app/src/main/java/kr/uk/dh/a20200317_02_java_first/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.uk.dh.a20200317_02_java_first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
