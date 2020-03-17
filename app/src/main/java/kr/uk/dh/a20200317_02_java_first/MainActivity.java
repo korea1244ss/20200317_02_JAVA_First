c9a5df982a5854871cebc521b8a3d3acbf4df392
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-17 15:18:28 +0900

    로그 출력 예제

--- app/src/main/java/kr/uk/dh/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/kr/uk/dh/a20200317_02_java_first/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
 import kr.uk.dh.a20200317_02_java_first.databinding.ActivityMainBinding;
@@ -22,7 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Log.d("버튼클릭" ,"입력 버튼 눌림");
+    binding.okBtn.setOnLongClickListener(new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            String input = binding.inputEdt.getText().toString();
 
-    binding.inputEdt
+            binding.resultText.setText(input);
+            binding.inputEdt.setText("");
+        }
+    });
+
+
+
+
     }
 }
